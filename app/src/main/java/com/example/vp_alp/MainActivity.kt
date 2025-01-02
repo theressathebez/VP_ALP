9652c355501d2e89f9c443e468a73aadc4dc0570
MattFA24 <153161493+MattFA24@users.noreply.github.com>
2025-01-02 22:39:12 +0700

    STTView STTViewModel and MainActivity

--- app/src/main/java/com/example/vp_alp/MainActivity.kt
+++ app/src/main/java/com/example/vp_alp/MainActivity.kt
@@ -1,22 +1,32 @@
 package com.example.vp_alp
 
-import StudyScroll
-import StudyView
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.ViewModelProvider
 import com.example.vp_alp.ui.theme.VP_ALPTheme
+import com.example.vp_alp.view.STTView
+import com.example.vp_alp.viewModel.STTViewModel
 
 class MainActivity : ComponentActivity() {
+    private lateinit var viewModel: STTViewModel
+
+    private val speechResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == RESULT_OK) {
+            viewModel.handleActivityResult(result.data)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        viewModel = ViewModelProvider(this).get(STTViewModel::class.java)
+        viewModel.setSpeechResultLauncher(speechResultLauncher)
         setContent {
             VP_ALPTheme {
-                StudyScroll()
+                STTView(viewModel = viewModel)
             }
         }
     }
 }
-

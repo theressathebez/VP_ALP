4bdf2a5e64417ca6d20eba762679ffb2eee068e4
nyanakumaro123 <nkisunyoto123@gmail.com>
2025-01-03 18:59:23 +0700

    asset + function login, register, profile view

--- app/src/main/java/com/example/vp_alp/view/ProfileView.kt
+++ app/src/main/java/com/example/vp_alp/view/ProfileView.kt
@@ -1,2 +1,17 @@
 package com.example.vp_alp.view
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Composable
+fun ProfileView() {
+
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ProfilePreview() {
+    ProfileView()
+}

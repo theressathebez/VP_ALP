c369ff212b6b42bc5151fff105a1fe91ed6aa773
nyanakumaro123 <nkisunyoto123@gmail.com>
2025-01-05 14:07:28 +0700

    Account, ChangePass, DelAccount, UserFlashcard View

---
+++ app/src/main/java/com/example/vp_alp/view/DelAccountView.kt
@@ -0,0 +1,17 @@
+package com.example.vp_alp.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Composable
+fun DelAccountView() {
+
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun DelAccountPreview() {
+    DelAccountView()
+}
---
+++ app/src/main/java/com/example/vp_alp/view/UserFlascardView.kt
@@ -0,0 +1,17 @@
+package com.example.vp_alp.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Composable
+fun UserFlashcardView() {
+
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun UserFlashcardPreview() {
+    UserFlashcardView()
+}

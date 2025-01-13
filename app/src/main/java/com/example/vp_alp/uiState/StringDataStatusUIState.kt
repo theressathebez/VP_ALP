9c3bf13ea77169a15ad7b060fd8ed6fe8e63b1f3
nyanakumaro123 <nkisunyoto123@gmail.com>
2025-01-13 19:38:17 +0700

    DeleteStringDataStatusUIState

--- app/src/main/java/com/example/vp_alp/uiState/StringDataStatusUIState.kt
+++
@@ -1,8 +0,0 @@
-package com.example.vp_alp.uiState
-
-sealed interface StringDataStatusUIState {
-    data class Success(val data: String): StringDataStatusUIState
-    object Start: StringDataStatusUIState
-    object Loading: StringDataStatusUIState
-    data class Failed(val errorMessage: String): StringDataStatusUIState
-}
